b6de14b1bed50f24acd3bf076339047dca615ea9
Abdulbaloch78600 <bk0109685@gmail.com>
2025-11-10 20:23:01 +0000

    updated Version

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 8000;
 // Routes
 app.get("/", (req, res) => res.send("Hello World"));
 app.get("/about", (req, res) => res.send("<h1>This is about page</h1>"));
+app.get("/contact",(req,res)=> res.send("<h1> For Quries Use the Given Email</1h><br> <h5> abc@gmail.com</h5>"))
+app.get("/date", (req, res) => {
+    res.send(`<p>Today the date is: <strong>${new Date().toDateString()}</strong></p>`);
+  });
+  
 
 // Start server
 app.listen(port, () => console.log(`Node server is running on port ${port}...`));
